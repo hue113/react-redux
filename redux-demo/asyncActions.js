29cb440734c237bed054c9f7a9fb089356e3f0d4
Hue Pham <huepham113@gmail.com>
2020-12-02 13:24:45 -0500

    Add asyncActions.js

---
+++ redux-demo/asyncActions.js
@@ -0,0 +1,82 @@
+const redux = require('redux')
+const thunkMiddleware = require('redux-thunk').default      // define async action creators
+const axios = require('axios')  
+
+const createStore = redux.createStore
+const applyMiddleware = redux.applyMiddleware
+
+const initialState = {
+    loading: false,
+    users: [],
+    error: ''
+}
+
+const FETCH_USERS_REQUEST = 'FETCH_USERS_REQUEST'   // fetch list of users
+const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS'   // fetched successfully
+const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE'   // error fetching the data
+
+const fetchUsersRequest = () => {
+    return {
+        type: FETCH_USERS_REQUEST
+    }
+}
+
+const fetchUsersSuccess = users => {
+    return {
+        type: FETCH_USERS_SUCCESS,
+        payload: users
+    }
+}
+  
+const fetchUsersFailure = error => {
+    return {
+        type: FETCH_USERS_FAILURE,
+        payload: error
+    }
+}
+
+const fetchUsers = () => {
+    return function (dispatch) {
+        dispatch(fetchUsersRequest())
+        axios
+            .get('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                // response.data is the users
+                const users = response.data.map(user => user.id)
+                dispatch(fetchUsersSuccess(users))
+            })
+            .catch(error => {
+                // error.message is the error message
+                dispatch(fetchUsersFailure(error.message))
+            })
+    }
+}
+
+const reducer = (state = initialState, action) => {
+    console.log(action.type)
+    switch (action.type) {
+        case FETCH_USERS_REQUEST:
+            return {
+                ...state,
+                loading: true
+            }
+        case FETCH_USERS_SUCCESS:
+            return {
+                loading: false,
+                users: action.payload,      // payload property that action contains
+                error: ''
+            }
+        case FETCH_USERS_FAILURE:
+            return {
+                loading: false,
+                users: [],      
+                error: action.payload       
+            }
+    }
+}
+
+
+// const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunkMiddleware))
+store.subscribe(() => { console.log(store.getState()) })
+store.dispatch(fetchUsers())

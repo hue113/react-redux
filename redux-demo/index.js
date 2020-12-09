31ddf1e39ba7ebfff0d320c28bedad62150c0073
Hue Pham <huepham113@gmail.com>
2020-12-08 20:39:19 -0500

    change dispatch

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -33,9 +33,9 @@ const reducer = (state = initialState, action) => {
 const store = createStore(reducer)
 console.log('initial state', store.getState())
 const unsubscribe = store.subscribe( () => console.log('Update state', store.getState())  ) 
-store.dispatch(buyCake())           // update state
-store.dispatch(buyCake())           // update state
-store.dispatch(buyCake())           // update state
+store.dispatch({type: 'BUY_CAKE'})          // update state 
+store.dispatch(buyCake())                   // update state
+store.dispatch(buyCake())                   // update state
 console.log('current state', store.getState())
 unsubscribe()                       // unsubscribe
 
